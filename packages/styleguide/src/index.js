dfadccd9195feb1f912280d200b0094542eb175d
Carlos Kazuo <ninetails.nine@gmail.com>
2018-09-06 16:04:20 -0300

    feat(styleguide): added initial catalog content

--- packages/styleguide/src/index.js
+++ packages/styleguide/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Catalog, pageLoader } from 'catalog'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <Catalog
+    title='Catalog'
+    pages={[
+      {
+        path: '/',
+        title: 'Introduction',
+        content: pageLoader('intro.md')
+      }
+    ]}
+  />,
+  document.getElementById('root')
+)
